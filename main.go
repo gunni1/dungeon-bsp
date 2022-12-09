3dd30b8bab2b1bdb02dd485f59fc72781fc26f0a
kubegu <guntram.kube@exxeta.com>
2022-12-09 15:57:50 +0100

    add command line flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"image/png"
 	"math/rand"
 	"os"
@@ -10,8 +11,17 @@ import (
 func main() {
 	rndSource := rand.NewSource(time.Now().UnixNano())
 	rnd := rand.New(rndSource)
-	root := Node{x: 0, y: 0, width: 40, height: 40}
-	root.SplitDeep(*rnd, 4)
+
+	var width int
+	var height int
+	var depth int
+	flag.IntVar(&width, "width", 40, "Total width of the area.")
+	flag.IntVar(&height, "height", 40, "total height of the area.")
+	flag.IntVar(&depth, "depth", 4, "Number of splits / depth of binary tree")
+	flag.Parse()
+
+	root := Node{x: 0, y: 0, width: width, height: height}
+	root.SplitDeep(*rnd, depth)
 	root.CreateLeafRooms(*rnd)
 	//Connect siblings
 
